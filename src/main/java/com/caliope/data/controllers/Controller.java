c37955e72d3a0d757cd9e2edcb0de257439ad594
fjrotgerl <fjrotgerl@gmail.com>
2019-04-12 10:47:17 +0200

    Limitado a ficheros .mp3

--- src/main/java/com/caliope/data/controllers/Controller.java
+++ src/main/java/com/caliope/data/controllers/Controller.java
@@ -1,5 +1,6 @@
 package com.caliope.data.controllers;
 
+import com.caliope.data.repositories.UsuarioRepository;
 import com.caliope.data.services.GestorArchivos;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,29 +9,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @org.springframework.stereotype.Controller
 public class Controller {
 
     private GestorArchivos gestorArchivos;
+    private UsuarioRepository usuarioRepository;
 
-    public Controller (GestorArchivos gestorArchivos) {
-        this.gestorArchivos = gestorArchivos;
+    public Controller (GestorArchivos gestorArchivos, UsuarioRepository usuarioRepository) {
+        this.gestorArchivos    = gestorArchivos;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @RequestMapping(value = "/subirCancion", method = RequestMethod.POST)
     public ResponseEntity<?> subirCancion(@RequestParam("file") MultipartFile file, @RequestParam("username") String username) {
 
-        if (file.isEmpty()) {
-            return new ResponseEntity<Object>("Seleccionar un archivo", HttpStatus.OK);
+        if (!this.usuarioRepository.findById(username).isPresent()) {
+            return new ResponseEntity<Object>("El usuario no se encuentra o no existe", HttpStatus.BAD_REQUEST);
         }
 
-        try {
-            this.gestorArchivos.subir(file,username);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (file == null || file.getSize() == 0 || file.getName().equals("") || file.isEmpty()) {
+            return new ResponseEntity<Object>("Selecciona un archivo", HttpStatus.BAD_REQUEST);
         }
+
+        if (!this.gestorArchivos.comprobarFormatosValidos(file)) {
+            return new ResponseEntity<Object>("Formato incorrecto", HttpStatus.BAD_REQUEST);
+        }
+
+        this.gestorArchivos.subir(file,username);
 
         return new ResponseEntity<Object>("Archivo subido correctamente", HttpStatus.OK);
 
--- src/main/java/com/caliope/data/services/GestorArchivos.java
+++ src/main/java/com/caliope/data/services/GestorArchivos.java
@@ -1,13 +1,19 @@
 package com.caliope.data.services;
 
+import com.caliope.data.repositories.UsuarioRepository;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GestorArchivos {
@@ -15,8 +21,14 @@ public class GestorArchivos {
     @Value("${fichero.subida}")
     private String directorio;
 
-    public void subir(MultipartFile file, String username) throws IOException {
-        if (!file.isEmpty()) {
+    private UsuarioRepository usuarioRepository;
+
+    public GestorArchivos(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void subir(MultipartFile file, String username) {
+        try {
             byte[] bytes = file.getBytes();
             String directorioSinArchivo = this.directorio + "//" + username + "//mis_canciones//";
             Path path = Paths.get(directorioSinArchivo);
@@ -30,9 +42,30 @@ public class GestorArchivos {
             }
 
             path = Paths.get(directorioSinArchivo + file.getOriginalFilename());
-
-            Files.write(path,bytes);
+            Files.write(path, bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public boolean comprobarFormatosValidos(MultipartFile file) {
+        List<String> formatos = new ArrayList<>();
+
+        formatos.add("mp3");
+
+        for (String formato : formatos) {
+            if (formato.equals(getFileExtension(file))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String getFileExtension(MultipartFile file) {
+
+        return FilenameUtils.getExtension(file.getOriginalFilename());
+
     }
 
 }
